d4a7aadaab9ebf9771c9d18afed3771e04dd6da3
MansoorCM <manzoorcm96@gmail.com>
2025-07-28 22:03:10 +0530

    Added contacts screen to display a list of contacts
    updated contact item layout properties

--- app/src/main/java/com/example/otpmanager/MainActivity.kt
+++ app/src/main/java/com/example/otpmanager/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.otpmanager.ui.ContactScreen
 import com.example.otpmanager.ui.theme.OTPManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello!",
-        modifier = modifier
-    )
+    ContactScreen()
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/otpmanager/ui/ContactScreen.kt
+++ app/src/main/java/com/example/otpmanager/ui/ContactScreen.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,24 +19,49 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.otpmanager.data.Contact
+import com.example.otpmanager.data.DummyContactsProvider
 
 @Composable
 fun ContactScreen(modifier: Modifier = Modifier) {
-
+    val contacts = DummyContactsProvider.dummyContacts
+    LazyColumn {
+        items(contacts) {
+            ContactItem(it)
+        }
+    }
 }
 
 @Composable
 fun ContactItem(contact: Contact, modifier: Modifier = Modifier) {
-    Surface(modifier = modifier) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier.padding(8.dp)
+    ) {
         Row(
-            horizontalArrangement = Arrangement.Center,
+            horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Image(Icons.Default.AccountCircle, "", modifier = Modifier.padding(8.dp))
-            Text("${contact.firstName} ${contact.lastName}", modifier = Modifier.padding(8.dp))
+            Image(
+                Icons.Outlined.AccountCircle, "",
+                modifier = Modifier
+                    .size(56.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                "${contact.firstName} ${contact.lastName}",
+                modifier = Modifier.padding(8.dp),
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun ContactsScreenPreview() {
+    ContactScreen()
 }
 
 @Preview

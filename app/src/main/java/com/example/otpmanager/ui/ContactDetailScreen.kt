f17be13ed770a185c195682fd1f13bb3b4fab2e2
MansoorCM <manzoorcm96@gmail.com>
2025-08-04 21:00:53 +0530

    Added ui for CreateContact screen

--- app/src/main/java/com/example/otpmanager/ui/ContactDetailScreen.kt
+++ app/src/main/java/com/example/otpmanager/ui/ContactDetailScreen.kt
@@ -7,10 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,50 +26,82 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.otpmanager.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactDetailScreen(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxSize()
-            .wrapContentSize()
-            .padding(16.dp)
-    ) {
-        var firstName by rememberSaveable { mutableStateOf("") }
-        OutlinedTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = { Text("First name") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text(
+                    stringResource(R.string.create_contact),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            },
+            navigationIcon = {
+                IconButton({}) {
+                    Icon(imageVector = Icons.Default.Clear, "")
+                }
+            },
+            actions = {
+                Button({}, modifier = Modifier.padding(16.dp)) {
+                    Text(stringResource(R.string.save))
+                }
+            }
         )
+    }) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxSize()
+                .wrapContentSize()
+                .padding(it)
+                .padding(16.dp)
+        ) {
+            var firstName by rememberSaveable { mutableStateOf("") }
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = { firstName = it },
+                label = { Text(stringResource(R.string.first_name)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-        var lastName by rememberSaveable { mutableStateOf("") }
-        OutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = { Text("Last name") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
+            var lastName by rememberSaveable { mutableStateOf("") }
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = { lastName = it },
+                label = { Text(stringResource(R.string.last_name)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-        var phone by rememberSaveable { mutableStateOf("") }
-        OutlinedTextField(
-            value = phone,
-            onValueChange = { phone = it },
-            label = { Text("Phone") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
+            var phone by rememberSaveable { mutableStateOf("") }
+            OutlinedTextField(
+                value = phone,
+                onValueChange = { phone = it },
+                label = { Text(stringResource(R.string.phone)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun ContactDetailScreenPreview() {
+    ContactDetailScreen()
 }

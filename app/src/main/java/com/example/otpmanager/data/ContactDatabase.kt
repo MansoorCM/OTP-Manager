7729a061ca25690fd2b77f87375d4a46ebd14f9f
MansoorCM <manzoorcm96@gmail.com>
2025-08-10 20:47:45 +0530

    Prepopulate db with dummy contacts data
    will be changed to only include real data

--- app/src/main/java/com/example/otpmanager/data/ContactDatabase.kt
+++ app/src/main/java/com/example/otpmanager/data/ContactDatabase.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [Contact::class], version = 1, exportSchema = false)
 abstract class ContactDatabase : RoomDatabase() {
@@ -18,8 +22,26 @@ abstract class ContactDatabase : RoomDatabase() {
             return INSTANCE ?: synchronized(this) {
                 Room.databaseBuilder(context, ContactDatabase::class.java, "contact_database")
                     .fallbackToDestructiveMigration(true)
+                    .addCallback(DatabaseCallback())
                     .build()
                     .also { INSTANCE = it }
+            }
+        }
+    }
+
+    private class DatabaseCallback() : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                CoroutineScope(Dispatchers.IO).launch {
+                    populateDb(database.contactDao())
+                }
+            }
+        }
+
+        suspend fun populateDb(contactDao: ContactDao) {
+            for (contact in DummyContactsProvider.dummyContacts) {
+                contactDao.insert(contact)
             }
         }
     }

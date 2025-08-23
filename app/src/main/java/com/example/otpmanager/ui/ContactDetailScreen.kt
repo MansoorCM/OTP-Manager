50dbde638b7550a4ecd0d99770e79bd8b8141525
MansoorCM <manzoorcm96@gmail.com>
2025-08-23 16:05:54 +0530

    Added TopAppBar with a back button in ContactDetailScreen

--- app/src/main/java/com/example/otpmanager/ui/ContactDetailScreen.kt
+++ app/src/main/java/com/example/otpmanager/ui/ContactDetailScreen.kt
@@ -5,10 +5,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -21,6 +29,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.otpmanager.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactDetailScreen(
     contactId: Int,
@@ -32,26 +41,34 @@ fun ContactDetailScreen(
     }
     val uiState by viewModel.uiState.collectAsState()
     val contact = uiState.contact
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxSize()
-            .wrapContentSize()
-    ) {
-        Text(
-            "${contact.firstName} ${contact.lastName}",
-            style = MaterialTheme.typography.headlineSmall
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(contact.phoneNum, style = MaterialTheme.typography.headlineSmall)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button({}) {
-            Text(stringResource(R.string.send_otp))
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button({}) {
-            Text(stringResource(R.string.delete_contact))
+
+    Scaffold(topBar = {
+        TopAppBar(
+            {},
+            navigationIcon = { IconButton({}) { Icon(Icons.AutoMirrored.Filled.ArrowBack, "") } })
+    }) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxSize()
+                .wrapContentSize()
+                .padding(it)
+        ) {
+            Text(
+                "${contact.firstName} ${contact.lastName}",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(contact.phoneNum, style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button({}) {
+                Text(stringResource(R.string.send_otp))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button({}) {
+                Text(stringResource(R.string.delete_contact))
+            }
         }
     }
 }

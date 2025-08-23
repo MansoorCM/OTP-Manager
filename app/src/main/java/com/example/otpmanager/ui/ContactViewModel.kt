7c45469846fdecf5eb709f89ae379afc69c9e702
MansoorCM <manzoorcm96@gmail.com>
2025-08-23 21:28:31 +0530

    update getContactById to use empty contact object when value is null

--- app/src/main/java/com/example/otpmanager/ui/ContactViewModel.kt
+++ app/src/main/java/com/example/otpmanager/ui/ContactViewModel.kt
@@ -67,7 +67,11 @@ class ContactViewModel(private val contactRepository: ContactRepository) : ViewM
     fun getContactById(id: Int) {
         viewModelScope.launch {
             contactRepository.getContactById(id).collect { contact ->
-                contact?.let {
+                if (contact == null) {
+                    _uiState.update { currentState ->
+                        currentState.copy(contact = Contact(0, "", "", ""))
+                    }
+                } else {
                     _uiState.update { currentState ->
                         currentState.copy(contact = contact)
                     }
